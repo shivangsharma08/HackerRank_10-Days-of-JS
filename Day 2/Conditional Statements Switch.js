4dd5d0529e0c7bdf946fa3c91a80dfd66687cb21
Shivang <44325395+shivangsharma08@users.noreply.github.com>
2021-06-05 17:37:12 +0530

    Added Day 2 Solutions

---
+++ Day 2/Conditional Statements Switch.js
@@ -0,0 +1,22 @@
+
+
+function getLetter(s) {
+    let letter;
+    switch (s[0]) {
+        case ('a' || 'e' || 'i' || 'o' || 'u' ):
+            letter = 'A';
+            break;
+        case  ('b' || 'c' || 'd' || 'f' || 'g'):
+            letter = 'B';
+            break;
+        case ('h' || 'j' || 'k' || 'l' || 'm'):
+            letter = 'C';
+            break;
+        case ('z' || 'n' || 'p' || 'q' || 'r' || 's' || 't' || 'v' || 'w' || 'x' || 'y' ):
+            letter = 'D';
+            break;
+        
+    }
+    return letter;
+}
+
---
+++ Day 2/Loops.js
@@ -0,0 +1,20 @@
+
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    let cons = [];
+    for (let i = 0; i < s.length; i++){
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            console.log(s[i]);
+        } else{
+            cons.push(s[i]);
+        }
+    }
+    for (let i = 0; i < cons.length; i++){
+        console.log(cons[i]);
+    }
+}
+
